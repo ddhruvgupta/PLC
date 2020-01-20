dfbe797eb442dd92aba926b4fcac430499c5464d
ddhruvgupta <ddhruvgupta@gmail.com>
2020-01-20 12:57:52 -0500

    added 'with' in parser and made sure 'if' still works

--- Homework 1/WAEParser.py
+++ Homework 1/WAEParser.py
@@ -1,40 +1,56 @@
 import ply.yacc as yacc
 from WAELexer import tokens
 
+# follows the order of tokens defined in WARLexar.py
+
+
 def p_waeStart(p):
-  'waeStart : wae SEMI'
-  p[0] = p[1]
+    'waeStart : wae SEMI'
+    p[0] = p[1]
+
 
 def p_wae_1(p):
-  'wae : NUMBER' 
-  p[0] = ['num',float(p[1])]
+    'wae : NUMBER'
+    p[0] = ['num', float(p[1])]
+
 
 def p_wae_2(p):
-  'wae : ID' 
-  p[0] = ['id',p[1]]
+    'wae : ID'
+    p[0] = ['id', p[1]]
+
 
 def p_wae_3(p):
-  'wae : LBRACE PLUS wae wae RBRACE' 
-  p[0] = ['+',p[3],p[4]]
+    'wae : LBRACE PLUS wae wae RBRACE'
+    p[0] = ['+', p[3], p[4]]
+
 
 def p_wae_4(p):
-  'wae : LBRACE MINUS wae wae RBRACE' 
-  p[0] = ['-',p[3],p[4]]
+    'wae : LBRACE MINUS wae wae RBRACE'
+    p[0] = ['-', p[3], p[4]]
+
 
 def p_wae_5(p):
-  'wae : LBRACE TIMES wae wae RBRACE' 
-  p[0] = ['*',p[3],p[4]]
+    'wae : LBRACE TIMES wae wae RBRACE'
+    p[0] = ['*', p[3], p[4]]
+
 
 def p_wae_6(p):
-  'wae : LBRACE DIV wae wae RBRACE' 
-  p[0] = ['/',p[3],p[4]]
+    'wae : LBRACE DIV wae wae RBRACE'
+    p[0] = ['/', p[3], p[4]]
+
 
 def p_wae_7(p):
-  'wae : LBRACE IF wae wae wae RBRACE'
-  p[0] = ['if',p[3],p[4],p[5]]
+    'wae : LBRACE IF wae wae wae RBRACE'
+    p[0] = ['if', p[3], p[4], p[5]]
+
+
+def p_wae_8(p):
+    'wae : LBRACE WITH wae wae wae RBRACE'
+    p[0] = ['with', p[3], p[4]]
+
 
 def p_error(p):
-  print("Syntax error in input!")
+    print("Syntax error in input!")
+
 
 parser = yacc.yacc()
-
