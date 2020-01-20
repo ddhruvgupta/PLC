8cc68d588a000151e1935c1f03aa3d8d7cb8521d
ddhruvgupta <ddhruvgupta@gmail.com>
2020-01-20 12:12:10 -0500

    initial files provided

---
+++ Homework 1/WAE.py
@@ -0,0 +1,66 @@
+from WAEParser import parser
+
+def eval_expression(tree):
+  if tree[0] == 'num':
+    return tree[1]
+  elif tree[0] == 'id':
+    return 'ERROR'
+  elif tree[0] == '+' or tree[0] == '-' or tree[0] == '*' or tree[0] == '/':
+    v1 = eval_expression(tree[1])
+    if v1 == 'ERROR':
+      return 'ERROR'
+    v2 = eval_expression(tree[2])
+    if v2 == 'ERROR':
+      return 'ERROR'
+    if tree[0] == '+':
+      return v1+v2
+    elif tree[0] == '-':
+      return v1-v2
+    elif tree[0] == '*':
+      return v1*v2
+    elif v2 != 0:
+      return v1/v2
+    else:
+      return 'ERROR'
+  else: #if clause
+    v1 = eval_expression(tree[1])
+    if v1 == 'ERROR':
+      return 'ERROR'
+    if v1 != 0:
+      return eval_expression(tree[2])
+    else:
+      return eval_expression(tree[3])
+
+def read_input():
+  result = ''
+  while True:
+    data = input('WAE: ').strip() 
+    if ';' in data:
+      i = data.index(';')
+      result += data[0:i+1]
+      break
+    else:
+      result += data + ' '
+  return result
+
+def main():
+  while True:
+    data = read_input() 
+    if data == 'exit;':
+      break
+    try:
+      tree = parser.parse(data)
+    except Exception as inst:
+      print(inst.args[0])
+      continue
+    #print(tree)
+    try:
+      answer = eval_expression(tree)
+      if answer == 'ERROR':
+        print('\nEVALUATION ERROR\n')
+      else:
+        print('\nThe value is '+str(answer)+'\n')
+    except:
+      pass
+ 
+main()
